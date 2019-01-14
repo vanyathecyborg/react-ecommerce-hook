dd491a0c92163a6c41df3fe745de2173552aa34b
Ivan <MUFNZ1@gmail.com>
2019-01-14 18:48:18 +0300

    fix tsconfig

--- src/actionTypes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.ADD_TO_CART = 'ADD_TO_CART';
-exports.REMOVE_FROM_CART = 'REMOVE_FROM_CART';
-exports.INCREASE_QUANTITY = 'INCREASE_QUANTITY';
-exports.DECREASE_QUANTITY = 'DECREASE_QUANTITY';
